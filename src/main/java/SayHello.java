93ab7c2f0dcd32171a110a31fff7073443129e11
helloway <simple_hlw@163.com>
2020-10-10 15:42:57 +0800

    add PersonA

--- src/main/java/SayHello.java
+++ src/main/java/SayHello.java
@@ -1,6 +1,9 @@
+import a.PersonA;
+
 public class SayHello {
     public static void main(String[] args){
-        System.out.println("Hello");
+        PersonA personA = new PersonA("A", 18);
+        System.out.println("Hello, I am " + personA.getName());
         System.out.println(100 + 200);
     }
 }
