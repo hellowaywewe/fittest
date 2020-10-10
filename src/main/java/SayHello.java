ec64f7926914f3e8c37732e036ff88fcf6727333
helloway <simple_hlw@163.com>
2020-10-10 15:46:32 +0800

    add PersonB

--- src/main/java/SayHello.java
+++ src/main/java/SayHello.java
@@ -1,9 +1,12 @@
 import a.PersonA;
+import a.PersonB;
 
 public class SayHello {
     public static void main(String[] args){
         PersonA personA = new PersonA("A", 18);
         System.out.println("Hello, I am " + personA.getName());
         System.out.println(100 + 200);
+        PersonB personB = new PersonB("B", 20);
+        System.out.println("Hello, I am " + personB.getName());
     }
 }
